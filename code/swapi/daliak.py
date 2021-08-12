8090c10771182184eabf67e41038fd5604a88e5e
Dalia Khammash <daliak@umich.edu>
2021-08-12 11:35:24 -0400

    retreive info about rey

---
+++ code/swapi/daliak.py
@@ -0,0 +1,6 @@
+import requests
+endpoint = 'https://swapi.py4e.com/api'
+response = requests.get(f"{endpoint}/people/", {'search': 'rey'}).json()
+rey = response['results'][0]
+
+print(f"\nDarth Vader = {rey}")
